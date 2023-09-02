7da9a7efb44f0c8da71adaab882e673bda22f5cc
Jetmir <Ahmati>
2023-09-02 07:55:18 +0200

    test commit

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert, StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
+
+ const onPressButton =()=> {
+    Alert.alert('You tapped the button!, this is test click');
+  }
+
   return (
     <View style={styles.container}>
       <Text>
         Open up App.js to start working on your app!
-        Open up App.js to start working on your app!
       </Text>
+
+        <View style={styles.buttonContainer}>
+          <Button onPress={onPressButton} title="Press Me" />
+        </View>
       <StatusBar style="auto" />
     </View>
   );
